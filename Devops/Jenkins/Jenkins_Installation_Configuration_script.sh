23332240443264eb2fbc0a1a47e183003d4ada9c
sourabh913 <40849235+sourabh913@users.noreply.github.com>
2024-09-04 00:25:57 +0530

    Update Jenkins_Installation_Configuration_script.sh

--- Devops/Jenkins/Jenkins_Installation_Configuration_script.sh
+++ Devops/Jenkins/Jenkins_Installation_Configuration_script.sh
@@ -84,3 +84,25 @@ only_crumb=$(echo ${arr_crumb[1]})
              -H 'Accept-Language: en,en-US;q=0.9,it;q=0.8' \
                --cookie $cookie_jar \
                 --data-raw "{'dynamicLoad':true,'plugins':['cloudbees-folder','antisamy-markup-formatter','build-timeout','credentials-binding','timestamper','ws-cleanup','ant','gradle','workflow-aggregator','github-branch-source','pipeline-github-lib','pipeline-stage-view','git','ssh-slaves','matrix-auth','pam-auth','ldap','email-ext','mailer'],'Jenkins-Crumb':'$only_crumb'}"
+
+#CONFIRMING JENKINS URL
+url=<your_jenkins_url>
+
+user=user
+password=password
+url_urlEncoded=$(python3 -c "import urllib.parse;print (urllib.parse.quote(input(), safe=''))" <<< "$url")
+
+cookie_jar="$(mktemp)"
+full_crumb=$(curl -u "$user:$password" --cookie-jar "$cookie_jar" $url/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+arr_crumb=(${full_crumb//:/ })
+only_crumb=$(echo ${arr_crumb[1]})
+
+curl -X POST -u "$user:$password" $url/setupWizard/configureInstance \
+  -H 'Connection: keep-alive' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H "$full_crumb" \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Accept-Language: en,en-US;q=0.9,it;q=0.8' \
+  --cookie $cookie_jar \
+  --data-raw "rootUrl=$url_urlEncoded%2F&Jenkins-Crumb=$only_crumb&json=%7B%22rootUrl%22%3A%20%22$url_urlEncoded%2F%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D&core%3Aapply=&Submit=Save&json=%7B%22rootUrl%22%3A%20%22$url_urlEncoded%2F%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D"
