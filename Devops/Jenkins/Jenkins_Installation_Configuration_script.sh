a3d30b53373b1bf563c0b1712b886dd294fdc309
sourabh913 <40849235+sourabh913@users.noreply.github.com>
2024-09-09 20:21:20 +0530

    done

--- Devops/Jenkins/Jenkins_Installation_Configuration_script.sh
+++ Devops/Jenkins/Jenkins_Installation_Configuration_script.sh
@@ -12,10 +12,13 @@ sudo apt install openjdk-17-jdk -y
 java -version
 
 # Add Jenkins repository key
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
 # Add Jenkins repository to sources list
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Update package lists
 sudo apt-get update
